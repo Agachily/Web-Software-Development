9bd196eb13c265e735ddba5c34406b6025aa45e1
Agachily <zetongzhao@foxmail.com>
2021-11-03 08:51:15 +0200

    push to heroku

--- Chapter 7/Assignment-7.7-1/app.js
+++ Chapter 7/Assignment-7.7-1/app.js
@@ -1,19 +1,17 @@
 import { listenAndServe } from "https://deno.land/std@0.113.0/http/server.ts";
 import { configure, renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts";
-import * as chatService from "./services/chatService.js"
+import * as messageService from "./services/messageService.js";
 
-let port = 7777
+let port = 7777;
 
-/**配置eta文件所在的目录 */
 configure({
-  views: `${Deno.cwd()}/views/`
-})
+  views: `${Deno.cwd()}/views/`,
+});
 
 const responseDetails = {
   headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
-/**配置用于重定向的函数 */
 const redirectTo = (path) => {
   return new Response(`Redirecting to ${path}.`, {
     status: 303,
@@ -21,48 +19,31 @@ const redirectTo = (path) => {
       "Location": path,
     },
   });
-}
+};
 
-/**对所获取到的数据 (result.rows) 进行处理和排序 */
-const processData = (data) => {
-  /**将数组进行反转，因为id越大表示数据越新 */
-  let newData = data.reverse()
-  if (newData.length > 5) {
-    newData = newData.slice(0, 5)
-  }
-  return newData
-}
-
-/**展示所获取到的数据和用于发送数据的表格 */
-const showData = async () => {
-  let result = await chatService.getData()
-  result = processData(result)
-
+const listMessages = async () => {
   const data = {
-    message: result
-  }
+    messages: await messageService.findLastFiveMessages(),
+  };
 
   return new Response(await renderFile("index.eta", data), responseDetails);
-}
+};
 
-/**添加新的消息 */
-const addData = async (request) => {
+const addMessage = async (request) => {
   const formData = await request.formData();
-  const sender = formData.get("sender")
-  const message = formData.get("message")
+  const sender = formData.get("sender");
+  const message = formData.get("message");
 
-  await chatService.create(sender, message)
-  return redirectTo("/")
-}
+  await messageService.create(sender, message);
+};
 
 const handleRequest = async (request) => {
-  const url = new URL(request.url);
-  if (request.method === "POST" && url.pathname === "/"){
-    return await addData(request)
-  } else {
-    return await showData(request)
+  if (request.method === "GET") {
+    return await listMessages();
+  } else if (request.method === "POST") {
+    await addMessage(request);
+    return redirectTo("/");
   }
 };
 
 listenAndServe(`:${port}`, handleRequest);
-
--- Chapter 7/Assignment-7.7-1/database/database.js
+++ Chapter 7/Assignment-7.7-1/database/database.js
@@ -12,31 +12,30 @@ const connectionPool = new Pool({
     port: 5432,
   }, CONCURRENT_CONNECTIONS);
 
-const executeQuery = async(query, ...args) => {
-    const response = {}
-    let client
-
-    try{
-        client = await connectionPool.connect()
-        const result = await client.queryObject(query, ...args)
-        if(result.rows){
-            response.rows = result.rows
-        }
+  const executeQuery = async (query, ...args) => {
+    const response = {};
+    let client;
+  
+    try {
+      client = await connectionPool.connect();
+      const result = await client.queryObject(query, ...args);
+      if (result.rows) {
+        response.rows = result.rows;
+      }
     } catch (e) {
-        response.error = e
+      response.error = e;
     } finally {
-        if (client) {
-          try {
-              await client.release()
-          } catch (e) {
-              console.log("Unable to release database connection.")
-              console.log(e)
-          }
+      if (client) {
+        try {
+          await client.release();
+        } catch (e) {
+          console.log("Unable to release database connection.");
+          console.log(e);
         }
+      }
     }
-
-    return response
-}
-
-/**导出executeQuery接口，用于为其它函数提供操作 */
-export { executeQuery }
+  
+    return response;
+  };
+  
+  export { executeQuery };
